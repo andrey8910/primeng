28487eb60c5620d6d1d783fe1c5217bb1caf315e
ivanov6444 <a.ivanov6444@gmail.com>
2022-10-31 13:10:37 +0200

    person height histogram

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule} from "@angular/common/http";
 import { AppComponent } from './app.component';
+import { BarchartComponent } from './barchart/barchart.component';
+import {ChartModule} from "primeng/chart";
+import { HttpClientService } from './services/http-client.service';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarchartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpClientService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/barchart/barchart.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClientService } from'../services/http-client.service';
+
+@Component({
+  selector: 'app-barchart',
+  templateUrl: './barchart.component.html',
+  styleUrls: ['./barchart.component.css']
+})
+export class BarchartComponent implements OnInit {
+  basicData: {
+    labels: string[],
+    datasets: {
+      label: string,
+      color: string,
+      data:  string[]
+    }[]
+  };
+  basicOptions: any;
+
+  public peoples : any[];
+  public peopleName: string[] = [];
+  public peopleHeight: string[] = []
+
+
+
+  constructor(private httpClientService: HttpClientService) {}
+
+  ngOnInit() {
+    this.httpClientService.getData().subscribe( (data: any) => {
+      this.peoples = data.results;
+      this.showPeoples()
+    })
+  }
+
+  showPeoples(){
+    this.peoples.forEach((person: any) => {
+      this.peopleName.push(person.name);
+      this.peopleHeight.push(person.height)
+    })
+
+    this.basicData = {
+      labels: this.peopleName,
+      datasets: [
+             {
+               label: 'height',
+               color: '#FFA726',
+               data: this.peopleHeight
+             }
+           ]
+         };
+  }
+}
